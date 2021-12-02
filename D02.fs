dbbb3afe87c6353d88f4c354745980abfcecf94a
Olivier Azeau <git@azeau.com>
2021-12-02 08:39:24 +0100

    Day 02

---
+++ D02.fs
@@ -0,0 +1,48 @@
+module aoc2021.D02
+
+open System
+
+type Position =
+  { Horizontal: int
+    Depth: int
+    Aim: int }
+
+type Command =
+  | Forward of int
+  | Down of int
+  | Up of int
+
+let Start: Position = { Horizontal = 0; Depth = 0; Aim = 0 }
+
+let Parse (command: string) : Command =
+  let words = command.Split(' ')
+  let arg = Int32.Parse(Array.item 1 words)
+
+  match Array.item 0 words with
+  | "forward" -> Forward arg
+  | "down" -> Down arg
+  | "up" -> Up arg
+  | _ -> failwith "Not a valid command"
+
+let Interpret1 (pos: Position) (command: Command) : Position =
+  match command with
+  | Forward h ->
+    { pos with
+        Horizontal = pos.Horizontal + h }
+  | Down d -> { pos with Depth = pos.Depth + d }
+  | Up d -> { pos with Depth = pos.Depth - d }
+
+let Interpret2 (start: Position) (command: Command) : Position =
+  match command with
+  | Forward x ->
+    { start with
+        Horizontal = start.Horizontal + x
+        Depth = start.Depth + start.Aim * x }
+  | Down x -> { start with Aim = start.Aim + x }
+  | Up x -> { start with Aim = start.Aim - x }
+
+let Travel interpreter (commands: string list) (start: Position) : Position =
+  List.map Parse commands
+  |> List.fold interpreter start
+
+let Result (position: Position) : int = position.Horizontal * position.Depth
---
+++ D02T.fs
@@ -0,0 +1,41 @@
+module aoc2021.D02T
+
+open System
+open NUnit.Framework
+open aoc2021.D02
+
+[<SetUp>]
+let Setup () = ()
+
+let parseCommandCases =
+  [ TestCaseData("down 5", Down 5)
+    TestCaseData("forward 8", Forward 8)
+    TestCaseData("up 3", Up 3) ]
+
+[<TestCaseSource("parseCommandCases")>]
+let TestParseCommand (input: string) (expectedCommand: Command) =
+  Assert.AreEqual(expectedCommand, Parse input)
+
+[<Test>]
+let TestExample () =
+  let input: string list =
+    [ "forward 5"
+      "down 5"
+      "forward 8"
+      "up 3"
+      "down 8"
+      "forward 2" ]
+
+  Assert.AreEqual(150, Start |> Travel Interpret1 input |> Result)
+  Assert.AreEqual(900, Start |> Travel Interpret2 input |> Result)
+
+[<Test>]
+let TestParts () =
+  let input =
+    Tools.inputForDay 2
+    |> Tools.splitLines
+
+  Assert.AreEqual(1636725, Start |> Travel Interpret1 input |> Result)
+  Assert.AreEqual(1872757425, Start |> Travel Interpret2 input |> Result)
+  
+  

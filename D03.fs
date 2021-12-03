391333130cdfa671c3bd75bceff3f83fbfe3aab0
Olivier Azeau <git@azeau.com>
2021-12-03 13:31:01 +0100

    Day 03

---
+++ D03.fs
@@ -0,0 +1,39 @@
+module aoc2021.D03
+
+open System
+
+let flip f a b = f b a
+let ReadBinary x = Convert.ToInt32(x, 2)
+let ReadBinaries xs = List.map ReadBinary xs
+let GetBit n x = (x >>> n) &&& 1
+let ExtractBits (n:int) : int list -> int list = List.map (GetBit n)
+let BitMax (xs:int list) : int = List.max xs |> float |> Math.Log2 |> Math.Floor |> int
+
+type Grouping<'key,'item> = 'key * 'item list
+type Sort<'a> = (Grouping<'a,'a> -> int) -> Grouping<'a,'a> list -> Grouping<'a,'a> list
+let CommonItem (sort:'a Sort) : xs: 'a list -> 'a*int =
+  List.groupBy id
+  >> sort (fun (_,l) -> List.length l)
+  >> List.head
+  >> (fun (v,l) -> (v,List.length l))
+
+let RateByCommonItemSelection (sort:int Sort) (xs:int list) : int =
+  let selectValue n = ExtractBits n xs |> CommonItem sort |> fst |> (flip (<<<) n)
+  [0..BitMax xs] |> List.map selectValue |> List.sum
+let GammaRate : int list -> int = RateByCommonItemSelection List.sortByDescending
+let EpsilonRate : int list -> int = RateByCommonItemSelection List.sortBy
+let PowerConsumption (xs:int list) : int = (GammaRate xs)*(EpsilonRate xs)
+
+let rec HighlanderIteration (xs: 'a list) (filters:('a list -> 'a list) list) : 'a =
+    let filtered = filters.Head xs
+    if filtered.Length = 1 then filtered.Head else (HighlanderIteration filtered filters.Tail)
+  
+let RatingByIterativeSelection (sort:int Sort) (valueOnTie: int) (xs:int list) : int =
+  let filterOnNthBit (n:int) (ys:int list) : int list =
+    let common,nb = ExtractBits n ys |> CommonItem sort
+    let chosen = if nb*2 = ys.Length then valueOnTie else common
+    List.filter (fun y -> GetBit n y = chosen) ys
+  List.map filterOnNthBit [BitMax xs..(-1)..0] |> HighlanderIteration xs
+let OxygenGeneratorRating : int list -> int = RatingByIterativeSelection List.sortByDescending 1
+let CO2ScrubberRating : int list -> int = RatingByIterativeSelection List.sortBy 0
+let LifeSupportRating (xs:int list) : int = (OxygenGeneratorRating xs)*(CO2ScrubberRating xs)
---
+++ D03T.fs
@@ -0,0 +1,61 @@
+module aoc2021.D03T
+
+open System
+open NUnit.Framework
+open aoc2021.D03
+
+[<SetUp>]
+let Setup () = ()
+
+[<TestCase("00100",4)>]
+[<TestCase("11110",30)>]
+let TestReadBinary (input: string) (expected: int) =
+  Assert.AreEqual(expected, ReadBinary input)
+
+[<TestCase("ABB",'B')>]
+[<TestCase("ABA",'A')>]
+let TestMostCommonItem (input: string) (expected: char) =
+  Assert.AreEqual(expected, Seq.toList input |> CommonItem List.sortByDescending |> fst)
+
+[<Test>]
+let TestShortExample () =
+  let input: string list =
+    [ "00100"
+      "11110"
+      "01010" ]
+
+  Assert.AreEqual([0;0;0], ReadBinaries input |> ExtractBits 0)
+  Assert.AreEqual([0;1;1], ReadBinaries input |> ExtractBits 1)
+  Assert.AreEqual([0;1;0], ReadBinaries input |> ExtractBits 4)
+
+[<Test>]
+let TestExample () =
+  let input: string list =
+    [ "00100"
+      "11110"
+      "10110"
+      "10111"
+      "10101"
+      "01111"
+      "00111"
+      "11100"
+      "10000"
+      "11001"
+      "00010"
+      "01010" ]
+
+  Assert.AreEqual(22, ReadBinaries input |> GammaRate)
+  Assert.AreEqual(9, ReadBinaries input |> EpsilonRate)
+  Assert.AreEqual(22*9, ReadBinaries input |> PowerConsumption)
+  Assert.AreEqual(23, ReadBinaries input |> OxygenGeneratorRating)
+  Assert.AreEqual(10, ReadBinaries input |> CO2ScrubberRating)
+  Assert.AreEqual(230, ReadBinaries input |> LifeSupportRating)
+
+[<Test>]
+let TestParts () =
+  let input =
+    Tools.inputForDay 3
+    |> Tools.splitLines
+
+  Assert.AreEqual(3242606, ReadBinaries input |> PowerConsumption)
+  Assert.AreEqual(4856080, ReadBinaries input |> LifeSupportRating)

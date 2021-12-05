58c9a79a3b533a9ce366a1bc82d927075be4e576
Olivier Azeau <git@azeau.com>
2021-12-05 12:08:17 +0100

    Day 05

---
+++ D05.fs
@@ -0,0 +1,45 @@
+module aoc2021.D05
+
+open System
+open Tools
+
+type Point =
+  { X : int
+    Y : int }
+  static member ReadFrom (s:string) : Point =
+    let ns = splitOnComma s |> List.map Int32.Parse in { X = ns.Head; Y = ns.Tail.Head }
+
+type Line =
+  { P1 : Point
+    P2 : Point }
+  static member ReadFrom (s:string) : Line =
+    let ps = splitOnSpaces s in { P1 = Point.ReadFrom ps.Head; P2 = Point.ReadFrom ps.Tail.Tail.Head }
+  member this.Xs () : int list = [ this.P1.X .. Math.Sign (this.P2.X - this.P1.X) .. this.P2.X ]
+  member this.Ys () : int list = [ this.P1.Y .. Math.Sign (this.P2.Y - this.P1.Y) .. this.P2.Y ]
+
+let allHVPoints (line:Line) : Point list =
+  if line.P1.X = line.P2.X then
+    List.map (fun y -> {X=line.P1.X; Y=y}) (line.Ys())
+  else
+    if line.P1.Y = line.P2.Y then
+      List.map (fun x -> {X=x; Y=line.P1.Y}) (line.Xs())
+    else
+      List.empty
+
+let allPoints (line:Line) : Point list =
+  let hvPoints = allHVPoints line
+  if hvPoints.Length = 0 then
+    List.zip (line.Xs()) (line.Ys()) |> List.map (fun (x,y) -> {X=x; Y=y})
+  else
+    hvPoints
+
+type Intersections = Map<Point,int>
+let findIntersections (pointFinder: Line -> Point list) (lines : Line list) : Intersections =
+  let addPoint (counters:Intersections) (point:Point) : Intersections =
+    let currentCount = counters.TryFind(point) |> Option.defaultValue 0
+    counters.Add(point, currentCount+1)
+  let addLine (counts:Intersections) (line:Line) : Intersections = List.fold addPoint counts (pointFinder line)
+  List.fold addLine (Intersections []) lines
+
+let countIntersections (counters : Intersections) : int =
+  Seq.where (fun x -> x>1) counters.Values |> Seq.length
---
+++ D05T.fs
@@ -0,0 +1,94 @@
+module aoc2021.D05T
+
+open NUnit.Framework
+open aoc2021.D05
+
+[<SetUp>]
+let Setup () = ()
+
+let allPointsHVCases =
+  [ TestCaseData({ P1={X=1;Y=1}; P2={X=1;Y=3} }, [{X=1;Y=1}; {X=1;Y=2}; {X=1;Y=3}])
+    TestCaseData({ P1={X=9;Y=7}; P2={X=7;Y=7} }, [{X=9;Y=7}; {X=8;Y=7}; {X=7;Y=7}]) ]
+[<TestCaseSource("allPointsHVCases")>]
+let TestAllHVPointsInLine (line: Line) (points: Point list) =
+  Assert.AreEqual(points, allHVPoints line)
+  Assert.AreEqual(points, allPoints line)
+
+let allPointsDiagCases =
+  [ TestCaseData({ P1={X=1;Y=1}; P2={X=3;Y=3} }, [{X=1;Y=1}; {X=2;Y=2}; {X=3;Y=3}])
+    TestCaseData({ P1={X=9;Y=7}; P2={X=7;Y=9} }, [{X=9;Y=7}; {X=8;Y=8}; {X=7;Y=9}]) ]
+[<TestCaseSource("allPointsDiagCases")>]
+let TestAllDiagPointsInLine (line: Line) (points: Point list) =
+  let empty : Point list = []
+  Assert.AreEqual(empty, allHVPoints line)
+  Assert.AreEqual(points, allPoints line)
+
+let sampleLines : Line list = [
+  { P1={X=0;Y=9}; P2={X=5;Y=9} }; 
+  { P1={X=8;Y=0}; P2={X=0;Y=8} }; 
+  { P1={X=9;Y=4}; P2={X=3;Y=4} }; 
+  { P1={X=2;Y=2}; P2={X=2;Y=1} }; 
+  { P1={X=7;Y=0}; P2={X=7;Y=4} }; 
+  { P1={X=6;Y=4}; P2={X=2;Y=0} }; 
+  { P1={X=0;Y=9}; P2={X=2;Y=9} }; 
+  { P1={X=3;Y=4}; P2={X=1;Y=4} }; 
+  { P1={X=0;Y=0}; P2={X=8;Y=8} }; 
+  { P1={X=5;Y=5}; P2={X=8;Y=2} }; 
+]
+
+[<Test>]
+let TestReadExample () =
+  let input = [
+    "0,9 -> 5,9"
+    "8,0 -> 0,8"
+    "9,4 -> 3,4"
+    "2,2 -> 2,1"
+    "7,0 -> 7,4"
+    "6,4 -> 2,0"
+    "0,9 -> 2,9"
+    "3,4 -> 1,4"
+    "0,0 -> 8,8"
+    "5,5 -> 8,2"
+  ]
+  Assert.AreEqual(sampleLines, List.map Line.ReadFrom input)
+
+let pointsCountCases =
+  [ TestCaseData([
+    0;0;0;0;0;0;0;1;0;0;
+    0;0;1;0;0;0;0;1;0;0;
+    0;0;1;0;0;0;0;1;0;0;
+    0;0;0;0;0;0;0;1;0;0;
+    0;1;1;2;1;1;1;2;1;1;
+    0;0;0;0;0;0;0;0;0;0;
+    0;0;0;0;0;0;0;0;0;0;
+    0;0;0;0;0;0;0;0;0;0;
+    0;0;0;0;0;0;0;0;0;0;
+    2;2;2;1;1;1;0;0;0;0;
+  ], allHVPoints, 5)
+    TestCaseData([
+    1;0;1;0;0;0;0;1;1;0;
+    0;1;1;1;0;0;0;2;0;0;
+    0;0;2;0;1;0;1;1;1;0;
+    0;0;0;1;0;2;0;2;0;0;
+    0;1;1;2;3;1;3;2;1;1;
+    0;0;0;1;0;2;0;0;0;0;
+    0;0;1;0;0;0;1;0;0;0;
+    0;1;0;0;0;0;0;1;0;0;
+    1;0;0;0;0;0;0;0;1;0;
+    2;2;2;1;1;1;0;0;0;0;
+  ], allPoints, 12) ]
+
+[<TestCaseSource("pointsCountCases")>]
+let TestCountsExample (expected:int list) (transform:Line -> Point list) (expectedIntersections : int) =  
+  let addCount (counts:Intersections) ((index,value):int*int) : Intersections =
+    if value = 0 then counts else counts.Add({X=index%10;Y=index/10}, value)
+  Assert.AreEqual(List.fold addCount (Intersections []) (List.indexed expected), findIntersections transform sampleLines)
+  Assert.AreEqual(expectedIntersections, findIntersections transform sampleLines |> countIntersections)
+
+[<Test>]
+let TestParts () =
+  let input = Tools.inputForDay 5 |> Tools.splitLines
+  let lines = List.map Line.ReadFrom input
+  Assert.AreEqual(4745, findIntersections allHVPoints lines |> countIntersections)
+  Assert.AreEqual(18442, findIntersections allPoints lines |> countIntersections)
+
